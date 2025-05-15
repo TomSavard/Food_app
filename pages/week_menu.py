0e85169c58a7de58ada751b5f001f6deaa794362
Tom Savard <tom.savard@doctolib.com>
2025-05-15 22:02:37 +0200

    passage de return vers st.rerun()

--- pages/week_menu.py
+++ pages/week_menu.py
@@ -29,7 +29,7 @@ def run(drive, folder_id):
         # Bouton suppression
         if cols[2].button("❌", key=f"del_menu_{idx}"):
             st.session_state.week_menu.pop(idx)
-            return
+            st.rerun()
         else:
             # Mise à jour de la ligne
             st.session_state.week_menu[idx] = {"recipe": selected, "note": note}
@@ -38,7 +38,7 @@ def run(drive, folder_id):
     # Bouton pour ajouter une ligne
     if st.button("Ajouter une recette"):
         st.session_state.week_menu.append({"recipe": "", "note": ""})
-        return
+        st.rerun()
 
 
     # Résumé de la liste de courses
