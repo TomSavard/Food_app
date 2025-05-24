1a676a05458eeb375deee354179537de2b4c9f3f
Tom Savard <tom.savard@doctolib.com>
2025-05-25 00:24:26 +0200

    ajout de la bdd

--- pages/2_ajouter_une_recette.py
+++ pages/2_ajouter_une_recette.py
@@ -23,7 +23,7 @@ folder_id = st.session_state.folder_id
 
 
 ingredient_db = load_ingredient_db(drive, folder_id)
-ingredient_names = ingredient_db["Nom"].dropna().unique().tolist() if not ingredient_db.empty else []
+ingredient_names = ingredient_db["alim_nom_fr"].dropna().unique().tolist() if not ingredient_db.empty else []
 st.write("Aperçu de la BDD d'ingrédients :")
 st.write(ingredient_db.head())
 
