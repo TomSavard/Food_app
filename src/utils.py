5297413037a862cb76a1adcd5a90075c9fd3d09f
Tom Savard <tom.savard@doctolib.com>
2025-05-25 13:27:30 +0200

    adding flexibility for units when computing macronutrients

--- src/utils.py
+++ src/utils.py
@@ -54,7 +54,7 @@ def compute_recipe_protein(ingredients, ingredient_db):
     total_protein = 0.0
     for ing in ingredients:
         # on check l'unité
-        unit = ing.unit.lower()
+        unit = ing.unit.lower().strip()
         st.write(f"Ingredient: {ing.name}, unit: '{unit}', quantity: {ing.quantity}")
         if unit == "g":
             qty = ing.quantity
